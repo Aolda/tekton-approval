d77bca9ce0a8cd6baf96a8b531f00bea90d53ee6
yeonnnnjs <sungyeon52@gmail.com>
2024-04-27 01:41:14 +0900

    chore: add metadata and og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>[Aolda] Tekton Approval</title>
+        <meta
+          name="description"
+          content="Tekton Approval in Aolda"
+          key="desc"
+        />
+      </Head>
+      <meta property="og:title" content={'[Aolda] Tekton Approval'} />
+      <meta property="og:description" content={'Tekton Approval in Aolda'} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={'public/og_image.png'} />
       <body>
         <Main />
         <NextScript />
